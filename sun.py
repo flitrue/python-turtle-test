683160c9ad93a740f8cefaa124400012d4fe6aef
flitrue <812863096@qq.com>
2019-05-10 18:15:26 +0800

    绘制太阳花

---
+++ sun.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#encoding=utf-8
+#author: flitrue
+
+import turtle as t
+
+t.screensize(400,400)
+t.setup(400,400)
+t.color("red", "yellow")
+t.hideturtle()
+t.pen
+t.speed(3)
+t.begin_fill()
+for _ in range(36):
+    t.forward(200)
+    t.left(170)
+t.end_fill()
+t.done()
