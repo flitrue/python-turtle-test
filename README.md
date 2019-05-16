4e77db1fb8e7ff20484cf0a887e75e0403facf23
flitrue <812863096@qq.com>
2019-05-16 10:06:58 +0800

    feat: 优化太阳花

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ turtle练习
 ## 案例一：小猪佩琦
 
 ![peppa-pig](images/peppa-pig.png)
+
+## 案例二：太阳花
+
+![sun-flower](images/sun-flower.png)
--- sun-flower.py
+++ sun-flower.py
@@ -3,16 +3,19 @@
 #author: flitrue
 
 import turtle as t
+import time
 
-t.screensize(400,400)
 t.setup(400,400)
 t.color("red", "yellow")
 t.hideturtle()
-t.pen
-t.speed(3)
+t.speed(6)
+t.penup()
+t.goto(-100, 0)
+t.pendown()
 t.begin_fill()
 for _ in range(36):
     t.forward(200)
     t.left(170)
+time.sleep(1)
 t.end_fill()
 t.done()
